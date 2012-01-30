cb706280d7822c8a0885c46640114e14b96831a1
Jeffrey Mullins <jmullins@30and30.com>
2012-01-30 15:09:31 -0500

    Refactored idl archetype to support "core.thrift" from idl-core.

--- src/main/resources/archetype-resources/idl-python/setup.py
+++ src/main/resources/archetype-resources/idl-python/setup.py
@@ -5,9 +5,12 @@ ${symbol_pound}!/usr/bin/env python
 
 from distutils.core import setup
 
-setup(name='${svcname}',
+setup(name='techresidents services ${svcname}',
       version='${project.version}',
       description='30and30 Service',
-      packages=['${svcname}']
+      packages=['techresidents',
+                'techresidents.services',
+                'techresidents.services.${svcname}'
+                'techresidents.services.${svcname}.gen']
     )
 
--- src/main/resources/archetype-resources/idl/__svcname__.thrift
+++ src/main/resources/archetype-resources/idl/__svcname__.thrift
@@ -1,11 +1,13 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-namespace java com.techresidents.services.${svcname}.thrift.gen
-namespace py ${svcname}
+namespace java com.techresidents.services.${svcname}.gen
+namespace py techresidents.services.${svcname}.gen
+
+include "core.thrift"
 
 service T${ServiceName}
 {
-    string getVersion(),
-    string getBuildNumber()
+    string getVersion(1:core.RequestContext requestContext),
+    string getBuildNumber(1:core.RequestContext requestContext)
 }
